3a1626821287f662ffc40ac9da3a80ee93d8e76e
Anton Novikov <tonn.post@gmail.com>
2019-04-29 12:44:30 +0400

    feat: use atom scheduler as said in etch-readme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { CompositeDisposable } from 'atom';
 import { HoverProvidersRegistry } from './atom-ide-hover';
 import { UI } from './linter';
+import etch from 'etch';
 
 import { LinterHoverProviderInstance } from './LinterHoverProvider';
 
@@ -8,6 +9,7 @@ class Main {
   private _subscriptions = new CompositeDisposable();
 
   activate(): void {
+    etch.setScheduler(atom.views);
   }
 
   deactivate(): void {
